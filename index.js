ec57d7b936cb63ac6a075693f40d7cfe08e6ce56
zahid4523114 <dewanmohammodzahidhasan@gmail.com>
2022-10-26 16:01:18 +0600

    update-custom-api-data

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const category = require("./category.json");
 const port = 3500;
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/courses", (req, res) => {
   res.send(courses);
 });
-
+app.get("/course/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const course = courses.find((c) => c.id === id);
+  res.send(course);
+});
+app.get("/category", (req, res) => {
+  res.send(category);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
