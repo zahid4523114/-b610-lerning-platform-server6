7f1eec389e9f80760b5db733b8ea4e3622529cdd
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-12-21 20:44:54 +0600

    retouch

--- index.js
+++ index.js
@@ -9,19 +9,23 @@ app.use(cors());
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
+
 app.get("/course/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const course = courses.find((c) => c.id === id);
   res.send(course);
 });
+
 app.get("/course/detail/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const course = courses.find((c) => c.id === id);
   res.send(course);
 });
+
 app.get("/category", (req, res) => {
   res.send(category);
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
